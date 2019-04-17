a1388deb38c19e470d7eecb4825c7316d45ef416
iZeaoGamer <jameshayward14@icloud.com>
2019-04-17 14:39:16 +0100

    !== MASSIVE UPDATE - 3.0.0 !==
    * NEW UPDATE AVAILABLE: v3.0.0
    * You can now customize your own messages.
    * Fixed internal server errors - closes #2 and closes 1
    * Fixed /warns from giving the wrong message, and not working properly.
    * Fixed /warn from not working properly.
    * Added bans database (If a user is banned, the bans folder will generate with the list of players that were banned, and for what reason it was for.
    * Fixed warnplayer plugin getting disabled due to a valid able not being null.
    * Added a customizable Players data option - Customize what the name of the data that generates when enabled. (Defaulted is Players/, but you may change it to whatever you want, but keep in mind - Make sure you know what you're doing before using this option.)
    * Fixed plugin enabling issues from occurring.
    * Added new option: Enable warn broadcast - Allows you to enable/disable warning broadcast.
    * Added back actions on which one will work, but instead, we've added it to onEnable() state to ensure that it'll work with the correct valid options.
    * Generate data message will commence if generate player data onJoin is enabled. (Configure messages in messages.yml)
    * Added cannot kick/ban ops message.
    * Fixed return types (to true), since it's a boolean, which requires a return value.
    * Fixed all bugs and improvements. Next update - Improvements to the plugin.

--- src/WarnPlayer/Main.php
+++ src/WarnPlayer/Main.php
@@ -1,137 +1,153 @@
-<?php
-
-  namespace WarnPlayer;
-  use pocketmine\plugin\PluginBase;
-  use pocketmine\event\Listener;
-  use pocketmine\utils\TextFormat as TF;
-  use pocketmine\Player;
-  use pocketmine\command\Command;
-  use pocketmine\command\CommandSender;
-  use pocketmine\utils\Config;
-  use pocketmine\event\player\PlayerJoinEvent;
-  use pocketmine\event\player\PlayerPreLoginEvent;
-  class Main extends PluginBase implements Listener {
-  public function onEnable() {
-  $this->getServer()->getPluginManager()->registerEvents($this, $this);
- if (!is_dir($this->getDataFolder())) { @mkdir($this->getDataFolder()); }
- if (!is_dir($this->getDataFolder() . "Players/")) { @mkdir($this->getDataFolder() . "Players"); }
- if (!is_file($this->getDataFolder() . "config.yml")) { $this->saveDefaultConfig(); }
- $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-    $this->config->set("action_after_three_warns: ", "kick");
-        $this->config->set("generate-player-data-on-join", true);
-        $this->config->set("require-reason", true);
-        $this->config->set("kick_reason", "&aYou have been kicked for being warned 3+ times.");
-    $this->config->set("max-warns", 3);
- $string = "action_after_three_warns: ";
-              $action = $this->config->get($string);
-
-              if($action === "kick") {
-                  $this->getLogger()->info("You chose the kick option. Enabling WarnPlayer.");
-              } elseif($action === "ban"){
-                  $this->getLogger()->info("You chose the ban option. Enabling WarnPlayer");
-        }
-    }
-    public function onJoin(PlayerJoinEvent $event){
-      $player = $event->getPlayer();
-      $this->config = new Config($this->getDataFolder() . "config.yml", CONFIG::YAML, array());
-       if($this->config->get("generate-player-data-on-join") === true){
-}
-if(!(file_exists($this->getDataFolder() . "Players/" . $player->getName() . ".txt"))) {
-  $this->getLogger()->info($player->getName() . " has been added to the warns database.");
-              touch($this->getDataFolder() . "Players/" . $player->getName() . ".txt");
-              file_put_contents($this->getDataFolder() . "Players/" . $player->getName() . ".txt", 0);
-            }
-}
-public function onPlayerBan(PlayerPreLoginEvent $event){
-    $player = $event->getPlayer();
-    if($player->isBanned()){
-        $player->close("", TF::colorize($this->config->get("ban_message"))); //to-do add a bans database.
-    }
-}
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
-      if(strtolower($cmd->getName()) === "warn") { 
-        if(!(isset($args[0]))){ //to-do implement a fully customisable messages system, where you can customise your very own messages right from the plugin.
-          $sender->sendMessage(TF::colorize("&6Error: not enough args. Usage: &b/warn <player> <reason>"));
-          return true;
-        } else {
-          $sender_name = $sender->getName();
-          $name = $args[0];
-          $player = $this->getServer()->getPlayer($name);
-          if($player === null) {
-            $sender->sendMessage(TF::colorize("&cPlayer &4" . $name . " &ccould not be found.")); 
-            return true;
-          } else {
-            unset($args[0]);
-            $player_name = $player->getName();
-            if(!(file_exists($this->getDataFolder() . "Players/" . $player_name . ".txt"))) {
-              touch($this->getDataFolder() . "Players/" . $player_name . ".txt");
-              file_put_contents($this->getDataFolder() . "Players/" . $player_name . ".txt", "0");
-            }
-if($this->config->get("require-reason") === true){
-            if(empty($args[1])){
-$sender->sendMessage(TF::colorize("&cYou need to enter a valid reason."));
-return true;
-}
-}
-            $reason = implode(" ", $args);
-            $file = file_get_contents($this->getDataFolder() . "Players/" . $player_name . ".txt");
-            $maxWarns = $this->config->get("max-warns");
-            if($file >= $maxWarns) { //To do make this configurable.
-              $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-              $action = $this->config->get("action_after_three_warns");
-              if($action === "kick") {
-                if($player->isOP()){
-                  return true;
-                }
-                $maxwarns = $this->config->get("max-warns");
-                $player->kick(TF::colorize($this->config->get("kick_message")));
-                $sender->sendMessage(TF::colorize("&b" . $player_name . " &6was kicked for being warned $maxwarns times."));
-                return true;
-              } else if($action === "ban") {
-                if($player->isOP()){
-                  return true;
-                }
-               $banList = $sender->getServer()->getNameBans();
-                $maxwarns = $this->config->get("max-warns");
-               $banList->addBan($player_name, $reason, null, $sender->getName());
-                $player->kick(TF::colorize($this->config->get("ban_message")));
-                $sender->sendMessage(TF::colorize("&b" . $player_name . " &6was banned for being warned $maxwarns times."));
-                return true;
-            } else {
-              $player->sendMessage(TF::colorize("&6You have been warned by &b" . $sender_name . " &6for &b" . $reason));
-              $this->getServer()->broadcastMessage(TF::colorize("&b" . $player_name . " &6was warned by &b" . $sender_name . " &6for &b" . $reason));
-              $file = file_get_contents($this->getDataFolder() . "Players/" . $player_name . ".txt");
-              file_put_contents($this->getDataFolder() . "Players/" . $player_name . ".txt", $file + 1);
-              $sender->sendMessage(TF::colorize("&6Warned &b" . $player_name . ", &6and added +1 warns to their file."));
-              return true;
-            }
-          }
-        }
-        }
-      }
-      if(strtolower($cmd->getName()) === "warns") {
-        if(!(isset($args[0]))) {
-          $sender->sendMessage(TF::colorize("&cError: not enough args. Usage: /warns <player>"));
-          return true;
-        } else {
-          $name = $args[0];
-          $player = $this->getServer()->getPlayer($name);
-          if($player === null) {
-            $sender->sendMessage(TF::colorize("&cPlayer &4" . $name . " &ccould not be found."));
-            return true;
-          } else {
-            $player_name = $player->getName();
-            if(!(file_exists($this->getDataFolder() . "Players/" . $player_name . ".txt"))) {
-              $sender->sendMessage(TF::colorize("&4" . $player_name . " &chas no warns."));
-              return true;
-            } else {
-              $player_warns = file_get_contents($this->getDataFolder() . "Players/" . $player_name . ".txt");
-              $sender->sendMessage(TF::colorize("&6Player &b" . $player_name . " &6has &b" . $player_warns . " &6warns."));
-              return true;
-            }
-          }
-        }
-return true;
-      }
-      }
-  }
+<?php
+
+  namespace WarnPlayer;
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\Player;
+  use pocketmine\command\Command;
+  use pocketmine\command\CommandSender;
+  use pocketmine\utils\Config;
+  use pocketmine\event\player\PlayerJoinEvent;
+  use pocketmine\event\player\PlayerPreLoginEvent;
+  class Main extends PluginBase implements Listener {
+  public function onEnable() {
+      if(!is_file($this->getDataFolder() . "config.yml")) { $this->saveDefaultConfig(); }
+      $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+ if (!is_dir($this->getDataFolder())) { @mkdir($this->getDataFolder()); }
+ if (!is_dir($this->getDataFolder() . $this->config->get("player_data") . "/")) { @mkdir($this->getDataFolder() . $this->config->get("player_data")); }
+ if(!is_dir($this->getDataFolder() . "Bans/")) mkdir($this->getDataFolder() . "Bans");
+ if(!is_file($this->getDataFolder() . "messages.yml")) { $this->saveResource("messages.yml");}
+ $this->messages = new Config($this->getDataFolder() . "messages.yml", Config::YAML, array());
+ $string = "action_after_three_warns: ";
+              $action = $this->config->get($string);
+
+              if($action === "kick") {
+                  $this->getLogger()->info("You chose the kick option. Enabling WarnPlayer.");
+              } elseif($action === "ban"){
+                  $this->getLogger()->info("You chose the ban option. Enabling WarnPlayer");
+              } elseif($action === null){
+                $this->getLogger()->error($action . " in file config.yml is invalid, valid options: kick, ban. Disabling plugin.");
+                $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin("WarnPlayer"));
+                return true;
+        }
+    }
+    public function onJoin(PlayerJoinEvent $event){
+      $player = $event->getPlayer();
+      $this->config = new Config($this->getDataFolder() . "config.yml", CONFIG::YAML, array());
+       if($this->config->get("generate-player-data-on-join") === true){
+       }
+if(!(file_exists($this->getDataFolder() . $this->config->get("player_data") . "/" . $player->getName() . ".txt"))) {
+  $this->getLogger()->info(str_replace(["{player}"], [$player->getName()], $this->messages->get("generate-data-message")));
+              touch($this->getDataFolder() . $this->config->get("player_data") . "/" . $player->getName() . ".txt");
+              file_put_contents($this->getDataFolder() . $this->config->get("player_data") . "/" . $player->getName() . ".txt", 0);
+            }
+}
+public function onPlayerBan(PlayerPreLoginEvent $event){
+    $player = $event->getPlayer();
+    if($player->isBanned()){
+        //if(!(file_exists($this->getDataFolder() . "Bans/") . $player->getName())){
+         //   touch($this->getDataFolder() . "Bans/" . $player->getName() . ".txt");
+           // file_put_contents($this->getDataFolder() . "Bans/" . $player->getName() . ".txt", "banned player: " . $player->getName()); //to-do add reason to the ban database
+        $player->close("", TF::colorize($this->messages->get("ban_message")));
+    }
+}
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+      if(strtolower($cmd->getName()) === "warn") { 
+        if(!(isset($args[0]))){
+          $sender->sendMessage(TF::colorize($this->messages->get("no-arguments")));
+          return true;
+        } else {
+          $sender_name = $sender->getName();
+          $name = $args[0];
+          $player = $this->getServer()->getPlayer($name);
+          if($player === null) {
+            $sender->sendMessage(TF::colorize($this->messages->get("player-not-found"))); //To-Do customise it to show variables. 
+            return true;
+          } else {
+            unset($args[0]);
+            $player_name = $player->getName();
+            if(!(file_exists($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt"))) {
+              touch($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt");
+              file_put_contents($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt", "0"); //To-Do customise playerdata generation (edit where the target should go to.)
+            }
+if($this->config->get("require-reason") === true){
+            if(empty($args[1])){
+$sender->sendMessage(TF::colorize($this->messages->get("valid-reason")));
+return true;
+}
+}
+          $reason = implode(" ", $args);
+            $file = file_get_contents($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt");
+            $maxWarns = $this->config->get("max-warns");
+            if($file >= $maxWarns) {
+              $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+              $action = $this->config->get("action_after_three_warns");
+              if($action === "kick") {
+                if($player->isOP()){
+                    $sender->sendMessage(TF::colorize($this->messages->get("cannot-kick-ops")));
+                  return true;
+                }
+                $maxwarns = $this->config->get("max-warns");
+                $player->kick(TF::colorize(str_replace(["{maxwarns}"], [$maxwarns], $this->config->get("kick_message"))));
+                $sender->sendMessage(TF::colorize(str_replace(["{player}", "{maxwarns}"], [$player->getName(), $maxwarns], $this->messages->get("kick-message"))));
+                return true;
+              } else if($action === "ban") {
+                if($player->isOP()){
+                    $sender->sendMessage(TF::colorize($this->messages->get("cannot-ban-ops")));
+                  return true;
+                }
+               $banList = $sender->getServer()->getNameBans();
+                $maxwarns = $this->config->get("max-warns");
+               $banList->addBan($player_name, $reason, null, $sender->getName());
+                $player->kick(TF::colorize(str_replace(["{maxwarns}"], [$maxwarns], $this->config->get("ban_message"))));
+                if(!(file_exists($this->getDataFolder() . "Bans/" . $player_name))){
+                    touch($this->getDataFolder() . "Bans/" . $player_name . ".txt");
+                    file_put_contents($this->getDataFolder() . "Bans/" . $player_name . ".txt", "Banned player: $player_name\nReason: $reason");
+                }
+                $sender->sendMessage(TF::colorize(str_replace(["{player}", "{maxwarns}"], [$player->getName(), $maxwarns], $this->messages->get("ban-message"))));
+                return true;
+              } elseif($action === null){
+                $this->getLogger()->error($action . " in file config.yml is invalid, valid options: kick, ban. Disabling plugin.");
+                $this->getServer()->getPluginManager()->disablePlugin($this->getServer()->getPluginManager()->getPlugin("WarnPlayer"));
+                return true;
+              }
+            } else {
+              $player->sendMessage(TF::colorize(str_replace(["{sender_name}", "{reason}"], [$sender_name, $reason], $this->messages->get("warned-message"))));
+              $file = file_get_contents($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt");
+              file_put_contents($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt", $file + 1);
+              $sender->sendMessage(TF::colorize(str_replace(["{player}"], [$player_name], $this->messages->get("added-warn-message"))));
+              if($this->config->get("enable-warnbroadcast-message") === true)
+                $this->getServer()->broadcastMessage(TF::colorize(str_replace(["{sender_name}", "{reason}"], [$sender_name, $reason], $this->messages->get("warn-broadcast-message"))));
+              return true;
+            }
+          }
+        }
+        }
+      if(strtolower($cmd->getName()) === "warns") {
+        if(!(isset($args[0]))){
+            $sender->sendMessage(TF::colorize($this->messages->get("warns-usage")));
+          return true;
+        } else {
+          $name = $args[0];
+          $player = $this->getServer()->getPlayer($name);
+          if($player === null){
+            $sender->sendMessage(TF::colorize(str_replace(["{player}"], [$name], $this->messages->get("player-not-found"))));
+            return true;
+          } else {
+            $player_name = $player->getName();
+            if(!(file_exists($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt"))) {
+              $sender->sendMessage(TF::colorize(str_replace(["{player}"], [$player_name], $this->messages->get("no-warns"))));
+              return true;
+            } else {
+              $player_warns = file_get_contents($this->getDataFolder() . $this->config->get("player_data") . "/" . $player_name . ".txt");
+              $sender->sendMessage(TF::colorize(str_replace(["{player}", "{warns}"], [$player_name, $player_warns], $this->messages->get("player-warns"))));
+              return true;
+            }
+          }
+        }
+    return true;
+  }
+  }
+  }
